9b5968e7b74c63f1a813cb6967628a6c019c2775
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-10-22 11:07:39 -0700

    Finished writing currency-service it can catch and report errors, and get api response when successful.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './services/currency-service';
+
+// Business Logic
+
+function getCurrency(selectCurrency)  {}
--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -1,5 +1,15 @@
 export default class CurrencyService  {
   static getWeather(selectCurrency) {
-    return  fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${selectCurrency}`)
+    return  fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${selectCurrency}`).then(function(response) {
+      if  (!response.ok)  {
+        const errorMessage = `${response.result} ${response.error-type}`;
+        throw new Error(errorMessage);
+      } else  {
+        return response.json();
+      }
+    })
+    .catch(function(error)  {
+      return error;
+    });                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
   }
 }
